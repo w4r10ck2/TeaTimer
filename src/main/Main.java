901a68eb17bf8be64712f86d737ea8d2adc8e3b5
w4r10ck2 <vogtand@fim.uni-passau.de>
2016-02-17 13:23:57 +0100

    added MenuBar - has no function yet

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,10 @@ package main;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,11 +13,25 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Timer");
-
-        StackPane root = new StackPane();
-        root.getChildren().addAll(new MainPane());
+        VBox root = new VBox();
         primaryStage.setScene(new Scene(root, 350, 250));
+        primaryStage.getIcons().add(new Image(getClass().getResource
+                ("Pictures/icon.jpg").toString()));
+        primaryStage.setResizable(false);
+        createMenu(primaryStage, root);
+        root.setSpacing(10);
+        root.getChildren().addAll(new MainPane());
         primaryStage.show();
+    }
+
+    private void createMenu(Stage primaryStage, VBox root) {
+        MenuBar menuBar = new MenuBar();
+        menuBar.prefWidthProperty().bind(primaryStage.widthProperty());
+        root.getChildren().add(menuBar);
+        Menu fileMenu = new Menu("File");
+        Menu settingsMenu = new Menu("Settings");
+        Menu helpMenu = new Menu("Help");
+        menuBar.getMenus().addAll(fileMenu, settingsMenu, helpMenu);
     }
 
 
