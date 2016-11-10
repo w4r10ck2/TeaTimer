97ce964e130d89c7ab7f81e3d94550f9f579627b
w4r10ck2 <andreas.vogt3@gmail.com>
2016-11-10 15:01:23 +0100

    fixed sizing in ChangeTimesPane

--- src/main/panes/ChangeTimesPane.java
+++ src/main/panes/ChangeTimesPane.java
@@ -7,6 +7,8 @@ import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 
 class ChangeTimesPane extends GridPane {
     private MainPane mainPane;
@@ -26,10 +28,13 @@ class ChangeTimesPane extends GridPane {
                 ("from"));
         ColumnConstraints column1 = new ColumnConstraints();
         column1.setPercentWidth(33);
+        column1.setMinWidth(mainPane.getWidth() * 0.33);
         ColumnConstraints column2 = new ColumnConstraints();
         column2.setPercentWidth(33);
+        column2.setMinWidth(mainPane.getWidth() * 0.33);
         ColumnConstraints column3 = new ColumnConstraints();
         column3.setPercentWidth(33);
+        column3.setMinWidth(mainPane.getWidth() * 0.33);
         getColumnConstraints().addAll(column1, column2, column3);
         setHgap(1);
         setVgap(1);
